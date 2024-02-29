51055ea22316fa7bb801a2659c4f55e6d838222e
Adam Surek <26888201+SuRektM8@users.noreply.github.com>
2024-02-29 12:09:14 -0500

    Added basic CLI for REPL testing

--- MonkeyInterpreter.CLI/Program.cs
+++ MonkeyInterpreter.CLI/Program.cs
@@ -1,10 +1,13 @@
-﻿namespace MonkeyInterpreter.Core;
+﻿using MonkeyInterpreter.REPL;
 
-public class Program
+namespace MonkeyInterpreter.CLI;
+
+public static class Program
 {
-	public void Main(string[] args)
+	public static void Main(string[] args)
 	{
 		Console.WriteLine("Enter Monkey commands");
 		
+		Repl.Start(Console.In, Console.Out);
 	}
 }
