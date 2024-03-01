f2711e1ddfb9bdd0926cfecc943351bb5bb75ec6
Adam Surek <26888201+SuRektM8@users.noreply.github.com>
2024-03-01 11:34:30 -0500

    Added base AST project with Tree and Parser base classes

---
+++ MonkeyInterpreter.AST/Parser.cs
@@ -0,0 +1,27 @@
+﻿using MonkeyInterpreter.AST;
+using MonkeyInterpreter.Core;
+
+namespace MonkeyInterpreter.AST;
+
+public class Parser
+{
+	public Lexer _Lexer;
+	public Token? CurrentToken;
+	public Token? PeekToken;
+
+	public Parser(Lexer lexer)
+	{
+		_Lexer = lexer;
+	}
+
+	public void NextToken()
+	{
+		CurrentToken = PeekToken;
+		PeekToken = _Lexer.NextToken();
+	}
+
+	public Tree.Program ParseProgram()
+	{
+		return new Tree.Program();
+	}
+}
---
+++ MonkeyInterpreter.AST/Tree.cs
@@ -0,0 +1,76 @@
+﻿using MonkeyInterpreter.Core;
+
+namespace MonkeyInterpreter.AST;
+
+public class Tree
+{
+	public interface INode
+	{
+		string TokenLiteral();
+	}
+	
+	public interface IStatement : INode
+	{
+		// INode Node { get; }
+		LetStatement StatementNode();
+	}
+	
+	public interface IExpression : INode
+	{
+		// INode Node { get; }
+		Identifier ExpressionNode();
+	}
+
+	public struct Identifier
+	{
+		public Token _Token;
+		public string Value;
+	}
+
+	public struct LetStatement
+	{
+		public Token _Token;
+		public Identifier Name;
+		public IExpression Value;
+	}
+	
+	public struct Program : IStatement, IExpression
+	{
+		public IStatement[] Statements;
+		public IExpression[] Expressions;
+	
+
+		public LetStatement StatementNode()
+		{
+			return new LetStatement();
+		}
+		
+		public Identifier ExpressionNode()
+		{
+			return new Identifier();
+		}
+		
+		public string TokenLiteral()
+		{
+			if (Statements.Length > 0)
+			{
+				return Statements[0].TokenLiteral();
+			}
+			
+			return "";
+		}
+
+		public string TokenLiteral(LetStatement statement)
+		{
+			return statement._Token.Literal;
+		}
+		
+		public string TokenLiteral(Identifier identifier)
+		{
+			return identifier._Token.Literal;
+		}
+	}
+
+	
+	
+}
