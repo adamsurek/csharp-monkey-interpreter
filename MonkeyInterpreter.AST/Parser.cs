6185c7b1109e5b373ab80fe6c54a46f95d314b64
Adam Surek <26888201+SuRektM8@users.noreply.github.com>
2024-03-07 19:44:37 -0500

    Added delegates and dictionaries for token to parsing function associations

--- MonkeyInterpreter.AST/Parser.cs
+++ MonkeyInterpreter.AST/Parser.cs
@@ -10,6 +10,12 @@ public class Parser
 	private Lexer _lexer;
 	private List<string> _errors;
 
+	private delegate IExpression ParsePrefixFunc();
+	private delegate IExpression ParseInfixFunc(IExpression expression);
+
+	private Dictionary<Token, ParsePrefixFunc> PrefixParsers;
+	private Dictionary<Token, ParseInfixFunc> InfixParsers;
+
 	public Parser(Lexer lexer)
 	{
 		_lexer = lexer;
@@ -127,5 +133,15 @@ public class Parser
 	private void PeekError(string token)
 	{
 		_errors.Add($"Expected next token to be {token}, got {_peekToken.Type} instead");
+	}
+
+	private void RegisterPrefixFunction(Token token, ParsePrefixFunc function)
+	{
+		PrefixParsers[token] = function;
+	}
+	
+	private void RegisterInfixFunction(Token token, ParseInfixFunc function)
+	{
+		InfixParsers[token] = function;
 	}
 }
