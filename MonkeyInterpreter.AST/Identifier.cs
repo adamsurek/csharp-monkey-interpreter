4e5dd8baea3c0e6154a82a0145204aae81d072bf
Adam Surek <26888201+SuRektM8@users.noreply.github.com>
2024-03-07 21:04:08 -0500

    Added constructor to Identifier class

--- MonkeyInterpreter.AST/Identifier.cs
+++ MonkeyInterpreter.AST/Identifier.cs
@@ -7,6 +7,12 @@ public struct Identifier : IExpression
 	public Token Token { get; set; }
 	public string Value { get; set; }
 	
+	public Identifier(Token token, string value)
+	{
+		Token = token;
+		Value = value;
+	}
+	
 	public string TokenLiteral()
 	{
 		return Token.Literal;
