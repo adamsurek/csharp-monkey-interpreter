19c9f9f89237d5dc26bd4eb04ab353fe1b787518
Adam Surek <26888201+SuRektM8@users.noreply.github.com>
2024-03-07 19:42:47 -0500

    Added test for AST stringify function

---
+++ MonkeyInterpreter.Tests/Parser/ASTTest.cs
@@ -0,0 +1,37 @@
+﻿using MonkeyInterpreter.AST;
+using MonkeyInterpreter.Core;
+using Xunit.Abstractions;
+
+namespace MonkeyInterpreter.Tests.Parser;
+
+public class ASTTest
+{
+	[Fact]
+	public void ASTToStringTest()
+	{
+		AbstractSyntaxTree ast = new()
+		{
+			Statements = new List<IStatement>
+			{
+				new LetStatement(new Token(Token.Let, "let"))
+				{
+					Name = new Identifier
+					{
+						Token = new Token(Token.Ident, "x"),
+						Value = "x"
+					},
+					Value = new Identifier
+					{
+						Token = new Token(Token.Ident, "y"),
+						Value = "y"
+					},
+				}
+			}
+		};
+		
+		Assert.Equal("let x = y;", ast.String());
+	}
+}
+
+
+
