49c60e52cbc00e1ae255eb6f1e353426b4acc594
Adam Surek <26888201+SuRektM8@users.noreply.github.com>
2024-03-11 21:55:59 -0400

    Created InfixExpression class inheriting from IExpression

--- MonkeyInterpreter.AST/InfixExpression.cs
+++ MonkeyInterpreter.AST/InfixExpression.cs
@@ -1,6 +1,32 @@
-﻿namespace MonkeyInterpreter.AST;
+﻿using System.Text;
+using MonkeyInterpreter.Core;
 
-public class InfixExpression
+namespace MonkeyInterpreter.AST;
+
+// TODO: Create proper constructor
+
+public class InfixExpression : IExpression
 {
-	
+	public Token Token;
+	public IExpression Left;
+	public IExpression Right;
+	public string Operator;
+
+	public string TokenLiteral()
+	{
+		return Token.Literal;
+	}
+
+	public string String()
+	{
+		StringBuilder stringBuilder = new();
+
+		stringBuilder.Append('(');
+		stringBuilder.Append(Left.String());
+		stringBuilder.Append(Operator);
+		stringBuilder.Append(Right.String());
+		stringBuilder.Append(')');
+
+		return stringBuilder.ToString();
+	}
 }
