255a1b333fff6b6f825ff397a4852c56d0209821
Adam Surek <26888201+SuRektM8@users.noreply.github.com>
2024-03-21 21:47:19 -0400

    Added IfExpression and BlockStatement classes and parser functions for each

---
+++ MonkeyInterpreter.AST/BlockStatement.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+using MonkeyInterpreter.Core;
+
+namespace MonkeyInterpreter.AST;
+
+public class BlockStatement : IExpression
+{
+	public Token Token;
+	public List<IStatement> Statements;
+
+	public BlockStatement(Token token, List<IStatement> statements)
+	{
+		Token = token;
+		Statements = statements;
+	}
+	public string TokenLiteral()
+	{
+		return Token.Literal;
+	}
+
+	public string String()
+	{
+		StringBuilder stringBuilder = new();
+
+		foreach (var statement in Statements)
+		{
+			stringBuilder.Append(statement.String());
+		}
+
+		return stringBuilder.ToString();
+	}
+}
---
+++ MonkeyInterpreter.AST/IfExpression.cs
@@ -0,0 +1,40 @@
+﻿using System.Text;
+using MonkeyInterpreter.Core;
+
+namespace MonkeyInterpreter.AST;
+
+public class IfExpression : IExpression
+{
+	public Token Token;
+	public IExpression? Condition;
+	public BlockStatement? Consequence;
+	public BlockStatement? Alternative;
+
+	public IfExpression(Token token)
+	{
+		Token = token;
+	}
+
+	public string TokenLiteral()
+	{
+		return Token.Literal;
+	}
+
+	public string String()
+	{
+		StringBuilder stringBuilder = new();
+
+		stringBuilder.Append("if");
+		stringBuilder.Append(Condition.String());
+		stringBuilder.Append(" ");
+		stringBuilder.Append(Consequence.String());
+
+		if (Alternative is not null)
+		{
+			stringBuilder.Append("else");
+			stringBuilder.Append(Alternative.String());
+		}
+
+		return stringBuilder.ToString();
+	}
+}
