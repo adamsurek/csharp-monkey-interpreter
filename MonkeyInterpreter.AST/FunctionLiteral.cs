6fc5bd1e33b3cc7aa02f49814e8429e0a76a5f8b
asurek <26888201+adamsurek@users.noreply.github.com>
2024-03-23 15:53:20 -0400

    Added FunctionLiteral class and required parser functions

---
+++ MonkeyInterpreter.AST/FunctionLiteral.cs
@@ -0,0 +1,40 @@
+﻿using System.Text;
+using MonkeyInterpreter.Core;
+
+namespace MonkeyInterpreter.AST;
+
+public class FunctionLiteral : IExpression
+{
+    public Token Token;
+    public List<Identifier>? Parameters = new();
+    public BlockStatement? Body;
+
+    public FunctionLiteral(Token token)
+    {
+        Token = token;
+    }
+
+    public string TokenLiteral()
+    {
+        return Token.Literal;
+    }
+
+    public string String()
+    {
+        StringBuilder stringBuilder = new();
+        List<string> parameters = new();
+            
+        foreach(Identifier parameter in Parameters)
+        {
+            parameters.Add(parameter.String());
+        }
+
+        stringBuilder.Append(TokenLiteral());
+        stringBuilder.Append('(');
+        stringBuilder.Append(string.Join(", ", parameters));
+        stringBuilder.Append(") ");
+        stringBuilder.Append(Body?.String());
+
+        return stringBuilder.ToString();
+    }
+}
