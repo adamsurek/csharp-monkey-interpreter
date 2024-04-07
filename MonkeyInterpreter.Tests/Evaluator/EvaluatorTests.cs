6527ce5e3ab17504a1b07876d0c7820f13e8090f
Adam Surek <26888201+SuRektM8@users.noreply.github.com>
2024-04-07 10:13:10 -0400

    Added Integer object evaluation tests

---
+++ MonkeyInterpreter.Tests/Evaluator/EvaluatorTests.cs
@@ -0,0 +1,48 @@
+﻿using System.Data;
+using MonkeyInterpreter.AST;
+using MonkeyInterpreter.Core;
+using Xunit.Abstractions;
+
+namespace MonkeyInterpreter.Tests.Evaluator;
+
+public class Program
+{
+	public Core.Lexer Lexer;
+	public AST.Parser Parser;
+	public AbstractSyntaxTree Ast;
+	
+	public Program(string expression)
+	{
+		Lexer = new Core.Lexer(expression);
+		Parser = new AST.Parser(Lexer);
+		Ast = Parser.ParseProgram();
+	}
+}
+
+public class GenericTests
+{
+	[Theory]
+	[InlineData("5", typeof(Integer))]
+	[InlineData("10", typeof(Integer))]
+	public void EvalResult_IsExpectedObjectType(string expression, Type expectedType)
+	{
+		Program program = new(expression);
+		IObject evaluatedObject = Evaluate(program);
+
+		Assert.IsType(expectedType, evaluatedObject);
+	}
+}
+
+public class IntegerEvaluationTests
+{
+	[Theory]
+	[InlineData("5", 5)]
+	[InlineData("10", 10)]
+	public void EvalResult_HasExpectedValue(string expression, int expectedValue)
+	{
+		Program program = new(expression);
+		Integer evaluatedObject = (Integer)Evaluate(program);
+
+		Assert.Equal(expectedValue, evaluatedObject.Value);
+	}
+}
